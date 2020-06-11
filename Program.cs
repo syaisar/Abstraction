c478ca64043437c1ed3df5b1fffaffabc96b66de
syaisar toumahu <61915350+syaisar@users.noreply.github.com>
2020-06-11 22:36:59 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Abstraction.AbstractionClass;
+using Abstraction.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Abstraction.AbstractClass;
+//using Abstraction.Interface;
+
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kendaraan kendaraan;
+            kendaraan = new PesawatTempur ();
+            kendaraan.Senjata();
+
+            Console.WriteLine();
+            kendaraan = new Kapal_Tempur ();
+            kendaraan.Senjata();
+
+            Console.WriteLine();
+            kendaraan = new Tank ();
+            kendaraan.Senjata();
+
+
+            /*Kendaraan kendaraan;
+            kendaraan = new Pesawat_Tempur();
+            kendaraan.Senjata();
+
+            Console.WriteLine();
+            kendaraan = new Kapal_Tempur();
+            kendaraan.Senjata();
+
+            Console.WriteLine();
+            kendaraan = new Tank();
+            kendaraan.Senjata()*/
+
+            Console.ReadKey();
+        }
+    }
+}
